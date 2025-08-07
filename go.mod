58b92c95f527acdfd10c35957faaaf40486bcda7
Darya Kabytova <dskabytova@avito.ru>
2025-08-07 19:48:18 +0300

    iter5

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/Skywardkite/service-metrics
 go 1.24.3
 
 require (
-	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.2.2
 	github.com/stretchr/testify v1.10.0
 	gotest.tools v2.2.0+incompatible
--- internal/config/serverConfig/serverConfig.go
+++ internal/config/serverConfig/serverConfig.go
@@ -3,25 +3,20 @@ package serverconfig
 import (
 	"errors"
 	"flag"
-
-	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	FlagRunAddr  	string	`env:"ADDRESS"`
+	FlagRunAddr  	string
 }
 
 func ParseFlags() (Config, error){
     var cfg Config
+    flag.StringVar(&cfg.FlagRunAddr, "a", ":8080", "address and port to run server")
+    flag.Parse()
 
-	err := env.Parse(&cfg)
-    if err != nil || cfg.FlagRunAddr == "" {
-		flag.StringVar(&cfg.FlagRunAddr, "a", ":8080", "address and port to run server")
-		flag.Parse()
-
-		if len(flag.Args()) > 0 {
-			return cfg, errors.New("Unknown flags")
-		}
+	if len(flag.Args()) > 0 {
+		return cfg, errors.New("Unknown flags")
 	}
+
     return cfg, nil
 }
