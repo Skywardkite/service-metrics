d5cd4f8ced3da81eb244c91f301c2243187e4c42
Darya Kabytova <dskabytova@avito.ru>
2025-08-11 13:54:00 +0300

    iter7

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -35,8 +35,6 @@ func main() {
     r.Post("/update/{metricType}/{metricName}/{metricValue}", h.UpdateHandler)
 	r.Get("/value/{metricType}/{metricName}", h.GetHandler)
 	r.Get("/", h.GetAllMetricsHandler)
-
-	//r.Post("/update", h.UpdateJSONHandler)
    	if err := http.ListenAndServe(cfg.FlagRunAddr, r); err != nil {
 		logger.Sugar.Fatalw("Error to listen server", err.Error(), "event", "start server")
 	}
